cb1e82c99bf51bacca940cefa86f8fa6a324379b
Yakub <yakubabdulmalik@gmail.com>
2023-11-14 12:27:13 +0100

    first commit

---
+++ practice.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+def greet():
+    name = input("What is your name? ")
+    print("Assalamu alikum wa rahmatullahi wa barakatuhu Ya", name)
+# In[6]:
+
+
+greet()
+
+
+# In[ ]:
+
+
+
+
